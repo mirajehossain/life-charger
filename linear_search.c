f726a1de1d9882715393dd25d7c698a4f63f3e5e
mirajehossain <mdalamin8521@gmail.com>
2017-06-09 00:08:07 +0600

    finally linear search ok

--- linear_search.c
+++ linear_search.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int a[100], b,i,key;
-    scanf("%d\n",&b);
-    for(i=0;i<b;i++){
-        scanf("%d",&a[i]);
-    }
-    scanf("%d",&key);
-    for(i=0;i<b;i++){
-        if(a[i]==key)
-           break;
-    }
-        if(i == b){
-            printf("Not found");
-        } else{
-            printf("%d",i+1); /// what location found my key
+    int a[100], b,i,key,casee=1;
+    while(scanf("%d\n",&b) !=EOF){
+        for(i=0;i<b;i++){
+            scanf("%d",&a[i]);
         }
+        scanf("%d",&key);
+        for(i=0;i<b;i++){
+            if(a[i]==key)
+               break;
+        }
+        printf("case %d : ", casee);
+        casee++;
+            if(i == b){
+                printf("Not found\n");
+            } else{
+                printf("%d\n",i+1); /// what location found my key
+            }
+    }
     return 0;
 }
