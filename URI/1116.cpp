26782261b02d4f724646030d8ab05e576fc64142
Alamin Hossain Miraje <mdalamin8521@gmail.com>
2018-05-28 01:39:14 +0600

    Accepted 1116

--- URI/1116.cpp
+++ URI/1116.cpp
@@ -4,25 +4,20 @@
 using namespace std;
  
 int main() {
-std::cout << std::setprecision(1) << std::fixed;
+       std::cout << std::setprecision(1) << std::fixed;
 
-   int n,x,y;
-   float res;
- 
+    int n,x,y;
     cin>>n;
-    res = 0;
-    for(int i=1;i<=n;i++){
-	cin>>x>>y;
-	if(y==0){
-	cout<<"divisao impossivel"<<endl;
-	continue;
-	} 
-	
-	res = (float)x/(float)y;
-	cout<<res<<endl;
-	
-  }
+    for(int i=0;i<n;i++){
+        cin>>x>>y;
+        if(y==0){
+       cout<<"divisao impossivel"<<endl;
+        } else {
+        float res;
+        res = (float)x/(float)y;
+               cout<<res<<endl;
+        }
+    }
  
-  
     return 0;
 }
