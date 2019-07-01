553feca1262625e1f87c4126499ffa087e4216fa
Md. Alamin (Miraje) <mdalamin8521@gmail.com>
2019-07-01 23:20:20 +0600

    Create insertion.cpp

---
+++ algorithom/sort/insertion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+	
+	int key;
+	for(int i = 1; i< n; i++) {
+		key = arr[i];
+		int j = i - 1;
+		while(j >= 0 && arr[j] > key){
+			int temp = arr[j+1];
+			arr[j + 1] = arr[j];
+			arr[j] = temp;
+			j = j -1;
+		}
+	}
+}
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i = 0; i< n; i++){
+		cin>>arr[i];
+	}
+	insertionSort(arr, n);
+	for(int i = 0; i< n; i++){
+		cout<<arr[i]<<" ";
+	}
+	
+	return 0;
+}
