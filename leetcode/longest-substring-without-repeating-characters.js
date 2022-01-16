1687ccc6707ba576bba384b89b06727da99dee22
Md. Alamin (Miraje) <miraajehossain@gmail.com>
2022-01-17 02:37:30 +0600

    Create longest-substring-without-repeating-characters.js

---
+++ leetcode/longest-substring-without-repeating-characters.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let finalMaxCount = 0;
+    let currentMaxCount = 0;
+    let set = new Set();
+    let j = 0;
+    for (let i = 0; i<s.length; i++) {
+        while(j < s.length) {
+            
+            if(!set.has(s[j])) {
+                set.add(s[j]);
+                finalMaxCount = Math.max(finalMaxCount, j - i + 1)
+                j++;
+            } else {
+                set.delete(s[i]);
+                break;
+            }
+        }
+    }
+    
+    return finalMaxCount;
+};
