9ae443289dd7f6950c15d8d0c67300809c7e0b4a
Md Alamin <miraajehossain@gmail.com>
2023-09-17 21:17:56 +0600

    plus one problem solved

--- leetcode/plus-one.js
+++ leetcode/plus-one.js
@@ -1,3 +1,4 @@
+// this solution not working for big int
 const plusOne = function(digits) {
     let n = 0;
     for (let i = 0; i < digits.length; i++) {
@@ -13,5 +14,24 @@ const plusOne = function(digits) {
     return finalArray.reverse();
 }
 
-console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
-// this solution not working for big int
+// console.log(plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
+
+const plusOneV2 = function(digits) {
+    let carry = 1;
+    for (let i = digits.length - 1; i >= 0; i-=1 ) {
+        const value = digits[i] + carry;
+        if (value < 10) {
+            carry = 0;
+            digits[i] = value;
+        } else {
+            carry = 1;
+            digits[i] = 0;
+        }
+    }
+
+    if (carry) {
+        digits.unshift(carry);
+    }
+    return digits;
+}
+console.log(plusOneV2([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
